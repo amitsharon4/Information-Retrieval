f3184929dd21b200c1c60f492dc679801111005b
Amit Sharon <amit.sharon@nice.com>
2022-08-08 14:29:18 +0300

    Split hyphenated words, removed numbers

--- vsm_ir.py
+++ vsm_ir.py
@@ -12,8 +12,8 @@ import sys
 import numpy as np
 import json
 
-#PART 1
-#create inverted index
+# PART 1
+# create inverted index
 
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)
@@ -27,6 +27,14 @@ docs_length = {}
 vsm = {}
 
 
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
 def count_words_in_record(record):
     global appearances
     ps = PorterStemmer()
@@ -35,8 +43,14 @@ def count_words_in_record(record):
     title = record.findNext('TITLE').text
     summary = record.findNext('ABSTRACT').text if record.findNext('ABSTRACT') is not None else record. \
         findNext('EXTRACT').text
-    stemmed_words = [ps.stem(w) for w in word_tokenize(title) + word_tokenize(summary)]
-    word_tokens = [w for w in stemmed_words if w not in stop_words]
+    screened_words = []
+    for w in word_tokenize(title) + word_tokenize(summary):
+        if '-' in w:
+            screened_words + w.split('-')
+        else:
+            screened_words.append(w)
+    stemmed_words = [ps.stem(w) for w in screened_words if not isfloat(w)]
+    word_tokens = [w.strip('-') for w in stemmed_words if w not in stop_words]
     docs_length[record_num] = len(word_tokens)
     for w in word_tokens:
         if w not in dictionary:
@@ -114,79 +128,101 @@ def create_index(directory):
                 }
     json_data["docs_dict"] = docs_dict
     json_data["words_dict"] = words_dict
-    json_data["avg_doc_length"] = sum(docs_length.values())/len(docs_length)
+    json_data["avg_doc_length"] = sum(docs_length.values()) / len(docs_length)
     with open('vsm_inverted_index.json', 'w', encoding='utf-8') as f:
         json.dump(json_data, f, ensure_ascii=False, indent=4)
 
-#create_index("C:/Users/galem/PycharmProjects/information_retrival")
+
+# create_index("C:/Users/galem/PycharmProjects/information_retrival")
 
 
-#PART 2
-#Query
+# PART 2
+# Query
 
-#BM25
-def bm25(fixed_query,docs_dict,words_dict,avgdl,N):
-    dict_res={}
-    b=0.75
-    k1=1.2
-    idf_query=idf_bm25_query(fixed_query, N, words_dict)
+# BM25
+def bm25(fixed_query, docs_dict, words_dict, avgdl, N):
+    dict_res = {}
+    b = 0.75
+    k1 = 1.2
+    idf_query = idf_bm25_query(fixed_query, N, words_dict)
+    print(idf_query)
     for doc in docs_dict.keys():
-        D =int(docs_dict[doc]["length"])#doc length
-        tf_doc_query=tf_bm25(doc,fixed_query,words_dict)
-        for_cal=(1-b+b*(D/avgdl))*k1
-        k1_1=k1+1
-        res=calc_sum_bm25(idf_query,tf_doc_query,for_cal,k1_1)
+        D = int(docs_dict[doc]["length"])  # doc length
+        print(D)
+        tf_doc_query = tf_bm25(doc, fixed_query, words_dict)
+        print(tf_doc_query)
+        for_cal = (1 - b + b * (D / avgdl)) * k1
+        print(for_cal)
+        k1_1 = k1 + 1
+        res = calc_sum_bm25(idf_query, tf_doc_query, for_cal, k1_1)
+        print(res)
+        exit()
 
-        if res > 0:
+        if res > 0.055:
             dict_res[doc] = res
-        sorted_dict = {k: v for k, v in sorted(dict_res.items(), key=lambda item: item[1])}
+        sorted_dict = {k: v for k, v in sorted(dict_res.items(), key=lambda item: item[1], reverse=True)}
 
     return sorted_dict
 
-def calc_sum_bm25(idf_query,tf_doc_query,for_cal,k1_1):
-    res=0
+
+def calc_sum_bm25(idf_query, tf_doc_query, for_cal, k1_1):
+    res = 0
     for i in range(len(idf_query)):
-        res+=(idf_query[i]*((tf_doc_query[i]*k1_1)/(tf_doc_query[i]+for_cal)))
+        res += (idf_query[i] * ((tf_doc_query[i] * k1_1) / (tf_doc_query[i] + for_cal)))
     return res
 
-def tf_bm25(doc,query,words_dict):
-    tf_query=[]
+
+def tf_bm25(doc, query, words_dict):
+    tf_query = []
     for word in query:
-        if words_dict[word] != None and  words_dict[word][doc] != None :
+        if words_dict.get(word) != None and words_dict.get(word).get(doc) != None:
             tf = float(words_dict[word][doc]["tf"])
         else:
-            tf=0
+            tf = 0
         tf_query.append(tf)
     return tf_query
 
 
-def idf_bm25_query(fixed_query,N,words_dict):
-    idf_query=[]
+def idf_bm25_query(fixed_query, N, words_dict):
+    print(N)
+    idf_query = []
     for q_i in fixed_query:
-        n_qi=len(words_dict.get(q_i))
-        idf_qi=math.log(1+ (N-n_qi+0.5)/(n_qi+0.5))
+        if words_dict.get(q_i) != None:
+            n_qi = len(words_dict.get(q_i))
+            print(n_qi)
+        else:
+            n_qi = 0
+        idf_qi = math.log(1 + (N - n_qi + 0.5) / (n_qi + 0.5))
         idf_query.append(idf_qi)
     return idf_query
 
 
-#TF - IDF
-#create vector similarity for query
-def tfidf_query(query_dict,words_dict,N): #create vector for query
-    #dict_res={}
-    res=[] #vector query
-    max_val_query=query_dict[max(query_dict, key=query_dict.get)]
-    for key in query_dict.keys(): #normal tf + calc tf*idf
-        val=query_dict[key]
-        if words_dict.get(key) != None:
-            idf=math.log2(N/len(words_dict.get(key)))
+# TF - IDF
+# create vector similarity for query
+def tfidf_query(query_dict, words_dict, N, doc):  # create vector for query
+    # dict_res={}
+    res = []  # vector query
+    v_norm = []
+    max_val_query = query_dict[max(query_dict, key=query_dict.get)]
+    for key in query_dict.keys():  # normal tf + calc tf*idf
+        val = query_dict[key]
+        if words_dict.get(key) != None and words_dict.get(key).get(doc) != None:
+            print(len(words_dict.get(key)))
+            idf = math.log2(N / len(words_dict.get(key)))
         else:
-            continue #the word not in corpus so we move on
-        #dict_res[key] = (val/max_val_query)*idf
-        res.append((val/max_val_query)*idf)
-    return res
+            idf1 = math.log2(N / 1)
+            v_norm.append((val / max_val_query) * idf1)
+            continue  # the word not in corpus so we move on
+        # dict_res[key] = (val/max_val_query)*idf
+        res.append((val / max_val_query) * idf)
+        v_norm.append((val / max_val_query) * idf)
+    print(v_norm)
+    res_norm = norm(v_norm)
+    return res, res_norm
 
-def tfidf_doc(query_dict,words_dict,doc): #create relevant vector for doc
-    vector_doc=[]
+
+def tfidf_doc(query_dict, words_dict, doc):  # create relevant vector for doc
+    vector_doc = []
     for q in query_dict.keys():
         if q not in words_dict.keys():
             continue
@@ -198,75 +234,76 @@ def tfidf_doc(query_dict,words_dict,doc): #create relevant vector for doc
     return vector_doc
 
 
+# calcualte cosin_similarity for doc and query
+def tfidf(fixed_query, docs_dict, words_dict, N):
+    dict_res = {}
+    query_dict = dict(Counter(fixed_query))  # dict of num of appearance of word in dict
+    print(query_dict)
 
-#calcualte cosin_similarity for doc and query
-def tfidf(fixed_query, docs_dict, words_dict,N):
-    dict_res={}
-    query_dict=dict(Counter(fixed_query)) #dict of num of appearance of word in dict
-    #print(query_dict)
-    vector_query=tfidf_query(query_dict,words_dict,N) #create vector similarity for query
-    vecotr_query_norm=norm(vector_query)
     for doc in docs_dict.keys():
-        vector_doc=tfidf_doc(query_dict,words_dict,doc)
-        vector_doc_norm=float(docs_dict[doc]["norm"])
-        print(vector_doc)
-        print(vector_query)
-        cosin_similarity=np.dot(vector_doc,vector_query)/(vector_doc_norm*vecotr_query_norm)
-        if cosin_similarity > 0:
-            dict_res[doc]= cosin_similarity
-    sorted_dict={k: v for k, v in sorted(dict_res.items(), key=lambda item: item[1])}
+        # print("doc num "+str(doc))
+        vector_query, vecotr_query_norm = tfidf_query(query_dict, words_dict, N,
+                                                      doc)  # create vector similarity for query
+        vector_doc = tfidf_doc(query_dict, words_dict, doc)
+        vector_doc_norm = float(docs_dict[doc]["norm"])
+        cosin_similarity = np.dot(vector_doc, vector_query) / (vector_doc_norm * vecotr_query_norm)
+        if cosin_similarity > 0.055:
+            dict_res[doc] = cosin_similarity
+    sorted_dict = {k: v for k, v in sorted(dict_res.items(), key=lambda item: item[1], reverse=True)}
     return sorted_dict
 
-#stem query
+
+# stem query
 def process_query(question):
-    q=question.lower()
+    q = question.lower()
     ps = PorterStemmer()
-    stop_words = stopwords.words('english') + ['.', "'", ".", "?", "!"]
+    stop_words = stopwords.words('english') + ['.', "'", ".", "?", "!", "[", "]", "(", ")"]
     stemmed_words = [ps.stem(w) for w in word_tokenize(q)]
     word_tokens = [w for w in stemmed_words if w not in stop_words]
     print(word_tokens)
     return word_tokens
 
-def query(ranking,question,index_path):
+
+def query(ranking, question, index_path):
     try:
         jason_inverted = open(index_path, "r")
     except:
         print("can't open json file")
         exit()
-    print("here")
     dict_inverted = json.load(jason_inverted)
-    print("next")
-    docs_dict=dict_inverted["docs_dict"]
-    words_dict=dict_inverted["words_dict"]
-    avgdl=float(dict_inverted["avg_doc_length"])
-
+    docs_dict = dict_inverted["docs_dict"]
+    words_dict = dict_inverted["words_dict"]
+    avgdl = float(dict_inverted["avg_doc_length"])
     jason_inverted.close()
-
-    fixed_query = process_query(question) #stem query as a list
-    N = len(docs_dict) #number of doc in corpus
-
+    fixed_query = process_query(question)  # stem query as a list
+    N = len(docs_dict)  # number of doc in corpus
     if ranking == "bm25":
-        relevant_doc=bm25(fixed_query,docs_dict,words_dict,avgdl,N)
+        relevant_doc = bm25(fixed_query, docs_dict, words_dict, avgdl, N)
     elif ranking == "tfidf":
-        relevant_doc=tfidf(fixed_query,docs_dict,words_dict,N)
+        relevant_doc = tfidf(fixed_query, docs_dict, words_dict, N)
     else:
         print("Worng argument for ranking")
         exit()
-
-    res = open("ranked_query_docs.txt","w")
+    res = open("ranked_query_docs.txt", "w")
+    print(relevant_doc)
+    max_val = relevant_doc[max(relevant_doc, key=relevant_doc.get)]
+    print("\n\n")
+    print(max_val)
+    print("\n\n")
     for doc in relevant_doc.keys():
-        res.write(doc+"\n")
-    res.close
+        res.write(doc + "\n")
+    res.close()
 
 
-#Main
+# Main
+"""
 def main(mode, path, ranking, question):
     if mode == "create_index":
         create_index(path)
     if mode == "query":
         query(ranking,question,path)
 """
-#Main
+# Main
 if len(sys.argv) == 1:
     print("Wrong number of arguments")
     exit()
@@ -285,7 +322,5 @@ if sys.argv[1] == "query":
         ranking = sys.argv[2]
         index_path = sys.argv[3]
         question = sys.argv[4]
-        query(ranking,question,index_path)
+        query(ranking, question, index_path)
         exit()
-
-"""
